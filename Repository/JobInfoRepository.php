a81a0749ee8cacb7c28725038b1d864a1106a105
Michiel Missotten <michiel@lil-web.fr>
2013-06-20 16:33:25 +0200

    Début d'intégration du l'application de gestion.

---
+++ Repository/JobInfoRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: michiel
+ * Date: 19/06/13
+ * Time: 17:08
+ */
+namespace Lilweb\JobBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Repository de gestion des entités.
+ */
+class JobInfoRepository extends EntityRepository
+{
+
+}
